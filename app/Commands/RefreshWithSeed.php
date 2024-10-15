e8de51716c34fccdd93af11143685def5514baca
Ricky Setiawan <rickiricki201533@gmail.com>
2024-10-15 10:57:17 +0700

    make seeder

---
+++ app/Commands/RefreshWithSeed.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+
+class RefreshWithSeed extends BaseCommand
+{
+    /**
+     * The Command's Group
+     *
+     * @var string
+     */
+    protected $group = 'Database';
+
+    /**
+     * The Command's Name
+     *
+     * @var string
+     */
+    protected $name = 'migrate:refresh-seed';
+
+    /**
+     * The Command's Description
+     *
+     * @var string
+     */
+    protected $description = 'Refresh migrations and run seeders.';
+
+    /**
+     * The Command's Usage
+     *
+     * @var string
+     */
+    protected $usage = 'command:name [arguments] [options]';
+
+    /**
+     * The Command's Arguments
+     *
+     * @var array
+     */
+    protected $arguments = [];
+
+    /**
+     * The Command's Options
+     *
+     * @var array
+     */
+    protected $options = [];
+
+    /**
+     * Actually execute a command.
+     *
+     * @param array $params
+     */
+    public function run(array $params)
+    {
+        // Run migrate:refresh
+        CLI::write('Refreshing migrations...', 'yellow');
+        $this->call('migrate:refresh', ['--all' => true]);
+
+        // Run db:seed DatabaseSeeder
+        CLI::write('Seeding the database...', 'yellow');
+        $this->call('db:seed', ['TestSeeder']);
+        
+        CLI::write('Migrations refreshed and seeders run successfully!', 'green');
+    }
+}
